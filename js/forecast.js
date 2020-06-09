0c9139a884d1c12cf9c4691c99445163e743ee9e
hrshainik <hrsshainik@gmail.com>
2020-06-10 00:28:52 +0600

    Second commit

--- js/forecast.js
+++ js/forecast.js
@@ -1,7 +1,7 @@
 const key = "WQvs2TtsK5IZJ0p1Hukk4oo62pJweNWV";
 
 const getWeather = async (id) => {
-  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const base = "https://dataservice.accuweather.com/currentconditions/v1/";
   const query = `${id}?apikey=${key}`;
 
   const response = await fetch(`${base}${query}`);
@@ -11,7 +11,7 @@ const getWeather = async (id) => {
 };
 
 const getCity = async (city) => {
-  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const base = "https://dataservice.accuweather.com/locations/v1/cities/search";
   const query = `?apikey=${key}&q=${city}`;
 
   const response = await fetch(`${base}${query}`);
